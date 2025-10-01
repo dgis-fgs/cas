c231502de52e2e984ca212c78ff006fec8a107aa
dgis-fgs <ferngauss@gmail.com>
2025-10-01 15:01:26 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+const CONFIG = {
+    DEEPSEEK_API_KEY: '%%DEEPSEEK_API_KEY%%', // Заменится при деплое
+    API_BASE_URL: 'https://api.deepseek.com/v1',
+    MODEL: 'deepseek-chat'
+};
